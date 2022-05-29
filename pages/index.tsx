4e3522b92e28e241347a1f09da32889869a99919
WhyAsh5114 <whyash5114@gmail.com>
2022-05-29 19:58:26 +0530

    designed a bit of the homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,35 @@
+import { NavBar } from './../components/NavBar';
+import { Highlight } from "@mantine/core";
+import Typewriter from 'typewriter-effect'
+
 export default function Home() {
-  return (<>
-    <h1>Hello World</h1>
-  </>);
-}
+  return (<div className="flex flex-col h-full">
+    <NavBar />
+
+    <div className="flex flex-col flex-1 justify-center">
+      <Highlight
+        align="center"
+        highlight={['WhyAsh\'s']}
+        highlightStyles={(theme) => ({
+          backgroundImage: theme.fn.linearGradient(45, theme.colors.cyan[5], theme.colors.indigo[5]),
+          fontWeight: 700,
+          WebkitBackgroundClip: 'text',
+          WebkitTextFillColor: 'transparent',
+        })}
+        className="text-5xl"
+      >
+        Hi, welcome to WhyAsh&apos;s
+      </Highlight>
+      <br /><br />
+      <div className="flex justify-center text-4xl font-mono text-white">
+        <Typewriter
+          options={{
+            strings: ['dev_life', 'blog', 'journal', 'projects'],
+            autoStart: true,
+            loop: true
+          }}
+        />
+      </div>
+    </div>
+  </div>);
+}  
