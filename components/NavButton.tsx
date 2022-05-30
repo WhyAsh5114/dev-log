d131bbd9eb97ab5e72b22c4652cbb52243287a1a
WhyAsh5114 <whyash5114@gmail.com>
2022-05-30 20:54:05 +0530

    added icon property to the NavButton.tsx

--- components/NavButton.tsx
+++ components/NavButton.tsx
@@ -5,12 +5,13 @@ import { ReactNode } from "react";
 interface props {
   link: string;
   children: ReactNode;
+  icon: ReactNode;
 }
 
 export default function NavButton(props: props) {
   return (
     <Link href={props.link}>
-      <Button variant="subtle" color="gray" className="flex-1 transition-colors font-medium text-lg" uppercase>
+      <Button leftIcon={props.icon} variant="subtle" color="gray" className="flex-1 transition-colors font-medium text-lg" uppercase>
         {props.children}
       </Button>
     </Link>);
