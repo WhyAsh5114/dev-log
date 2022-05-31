829aecf214fdd45612375475cd8b17f5177c1545
WhyAsh5114 <whyash5114@gmail.com>
2022-05-31 19:35:18 +0530

    merged 2 components was unnecessary to have 2 seperate files for navigation bar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,12 +1,30 @@
+import { Burger, Drawer } from "@mantine/core";
+import { useState } from "react";
 import NavButtonGroup from "./NavButtonGroup";
-import NavDrawer from "./NavDrawer";
 
 export function NavBar() {
+  const [opened, setOpened] = useState(false);
+
   return (<>
     <nav className="hidden gap-2 py-3 md:flex" data-cy="desktop-navbar">
       <NavButtonGroup />
     </nav>
-    <NavDrawer />
+    <div className="md:hidden">
+      <Drawer
+        opened={opened}
+        onClose={() => setOpened(false)}
+        size="sm"
+        position="left"
+      >
+        <nav className="flex flex-col gap-5">
+          <NavButtonGroup />
+        </nav>
+      </Drawer>
+
+      <div className="flex w-full justify-center my-1">
+        <Burger opened={false} onClick={() => setOpened(!opened)} aria-label="Open Navigation"/>
+      </div>
+    </div>
   </>);
 }
   
