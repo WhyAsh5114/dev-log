783e27e07cd2323f9f7b3bc8e3ab9291c23c70f7
WhyAsh5114 <whyash5114@gmail.com>
2022-06-02 01:10:05 +0530

    added the 2nd page image bg, and made WelcomeText a component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,51 @@
+import { WelcomeText } from './../components/WelcomeText';
 import { NavBar } from './../components/NavBar';
-import { Highlight } from "@mantine/core";
-import Typewriter from 'typewriter-effect'
+import { Divider, Highlight } from "@mantine/core";
 import Description from '../components/Description';
 
 export default function HomePage() {
   return (<>
-  <div className="flex flex-col h-full">
-    <NavBar />
+    <div className="flex flex-col h-full">
+      <NavBar />
+      <div 
+        className='flex h-full items-center gap-2 flex-col md:flex-row justify-evenly' 
+        style={{
+          backgroundImage: `url('/layered-waves-haikei.svg')`, 
+          backgroundRepeat: 'no-repeat', 
+          backgroundSize: 'cover'
+        }}
+      >
+        <WelcomeText />
+        <Description />
+      </div>
+    </div>
 
-    <div className='flex h-full items-center gap-2 flex-col md:flex-row justify-evenly' style={{backgroundImage: `url('/layered-waves-haikei.svg')`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}}>
-      <div className="flex flex-col justify-center mt-12 md:mt-4 lg:flex-[1.5]">
-        <Highlight
-          align="center"
-          highlight={['WhyAsh\'s']}
-          highlightStyles={(theme) => ({
-            backgroundImage: theme.fn.linearGradient(45, theme.colors.cyan[5], theme.colors.indigo[5]),
+    <Divider size={'sm'} color='indigo'></Divider>
+
+    <div 
+      className='h-full' 
+      style={{
+        backgroundImage: `url('/layered-peaks-haikei.svg')`, 
+        backgroundRepeat: 'no-repeat', 
+        backgroundSize: 'cover'
+      }}
+    >
+      <div className='flex w-full md:w-9/12 float-right h-full flex-col justify-center px-8 gap-5'>
+        <Highlight 
+          highlight={['Projects']} 
+          highlightStyles={theme => ({
+            backgroundImage: theme.fn.linearGradient(45, theme.colors.indigo[7], theme.colors.indigo[3]),
             WebkitBackgroundClip: 'text',
-            WebkitTextFillColor: 'transparent',
-          })}
-          className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-semibold"
+            WebkitTextFillColor: 'transparent'
+          })} 
+          className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-semibold text-right px-4"
         >
-          Welcome to WhyAsh&apos;s
+          My Projects
         </Highlight>
-        <div className="h-3 md:h-5 lg:h-7 xl:h-9"/>
-        <div className="flex justify-center text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-mono text-white">
-          <Typewriter
-            options={{
-              strings: ['dev_life', 'blog', 'journal', 'projects'],
-              autoStart: true,
-              loop: true
-            }}
-          />
+        <div className='flex h-5/6 bg-zinc-800 rounded-3xl'>
+          
         </div>
       </div>
-      <Description />
-    </div>
-  </div>
-  <div className='h-full'>
-      <p>PARAGRAPH TAG IS HERE BOII</p>
     </div>
   </>);
 }  
