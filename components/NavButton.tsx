f02bfdfb4bc3a1f709ab3b0ab4bd6461eaca7770
WhyAsh5114 <whyash5114@gmail.com>
2022-06-02 01:14:45 +0530

    data-cy won't work directly, need to pass as a prop

--- components/NavButton.tsx
+++ components/NavButton.tsx
@@ -7,12 +7,20 @@ interface props {
   children: ReactNode;
   icon: ReactNode;
   hoverClass: string;
+  data_cy: string;
 }
 
 export default function NavButton(props: props) {
   return (
     <Link href={props.link}>
-      <Button leftIcon={props.icon} variant="subtle" color="gray" className={`flex-1 transition-colors font-medium text-lg ${props.hoverClass}`} uppercase>
+      <Button 
+        leftIcon={props.icon} 
+        variant="subtle" 
+        color="gray" 
+        className={`flex-1 transition-colors font-medium text-lg ${props.hoverClass}`} 
+        uppercase 
+        data-cy={props.data_cy}
+      >
         <Text>{props.children}</Text>
       </Button>
     </Link>
