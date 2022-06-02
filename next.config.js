31a62e4b896a6f81e544d16e4e6f7c871ed0469c
WhyAsh5114 <whyash5114@gmail.com>
2022-06-02 21:04:56 +0530

    added ability to load mdx in react

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+    providerImportSource: "@mdx-js/react",
+  },
+})
+
+const nextConfig = withMDX({
   reactStrictMode: true,
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
   },
-}
+})
 
 module.exports = nextConfig
