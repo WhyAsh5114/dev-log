b099ce0334b60413611c0c0b7f9653122f1829be
WhyAsh5114 <whyash5114@gmail.com>
2022-06-10 10:38:59 +0530

    added transitions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,33 @@
-import { Affix, Divider, Highlight, Transition, Button, Title, Text, List, ThemeIcon } from "@mantine/core";
-import { useWindowScroll } from '@mantine/hooks';
-import { ArrowUp, CircleCheck } from "tabler-icons-react";
+import {
+  Divider,
+  Highlight,
+  Transition,
+  Title,
+  Text,
+  List,
+  ThemeIcon,
+  GroupedTransition,
+} from "@mantine/core";
+import { CircleCheck } from "tabler-icons-react";
 import { NavBar } from "./../components/navigation/NavBar";
 import Typewriter from "typewriter-effect";
 import Proj1 from "./../data/projects/next_cryptocurrencies_handbook.mdx";
-import Proj2 from "./../data/projects/dota2_rampage_tracker.mdx"
+import Proj2 from "./../data/projects/dota2_rampage_tracker.mdx";
+import { useEffect, useState } from "react";
 
 const components = {
-  h1: (props: any) => <h2 className="font-bold text-xl md:text-2xl lg:text-3xl xl:text-4xl" {...props}/>,
-  p: (props: any) => <p className="text-sm md:text-base" {...props} />
-}
+  h1: (props: any) => (
+    <h2
+      className="font-bold text-xl md:text-2xl lg:text-3xl xl:text-4xl"
+      {...props}
+    />
+  ),
+  p: (props: any) => <p className="text-sm md:text-base" {...props} />,
+};
 
 export default function HomePage() {
-  const [scroll, scrollTo] = useWindowScroll();
-
   return (
     <>
-      <Affix position={{ bottom: 20, right: 20 }}>
-        <Transition transition="slide-up" mounted={scroll.y > 0}>
-          {(transitionStyles) => (
-            <Button
-              leftIcon={<ArrowUp />}
-              style={transitionStyles}
-              onClick={() => scrollTo({ y: 0 })}
-              className="bg-indigo-700"
-            >
-              Home
-            </Button>
-          )}
-        </Transition>
-      </Affix>
       <div className="flex flex-col h-full">
         <NavBar />
         <div
@@ -54,7 +52,7 @@ export default function HomePage() {
           backgroundImage: `url('/layered-peaks-haikei.svg')`,
           backgroundRepeat: "no-repeat",
           backgroundSize: "cover",
-          backgroundAttachment: 'fixed'
+          backgroundAttachment: "fixed",
         }}
       >
         <div className="flex w-full md:w-2/3 flex-col justify-center p-8 gap-5 ml-auto h-full mb-5 mr-0 md:mr-6 lg:mr-12 xl:mr-20 mt-3">
@@ -76,7 +74,7 @@ export default function HomePage() {
           <div className="flex bg-stone-900 rounded-lg flex-col gap-2 p-4 md:p-6 lg:p-8 xl:p-10">
             <Proj1 components={components} />
           </div>
-          <Divider size='sm' color='indigo' className="rounded-full my-10" />
+          <Divider size="sm" color="indigo" className="rounded-full my-10" />
           <div className="flex bg-stone-900 rounded-lg flex-col gap-2 p-4 md:p-6 lg:p-8 xl:p-10">
             <Proj2 components={components} />
           </div>
@@ -93,33 +91,67 @@ export default function HomePage() {
   );
 }
 
-
 function Description() {
+  const duration = 1000;
+
+  let [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
   return (
-    <div className="flex lg:flex-1 justify-center flex-col border-l-2 pl-3 h-min py-5">
-      <Title className="h-min text-xl md:text-2xl lg:text-3xl">
-        Hi, my name is Yash
-      </Title>
-      <Text className="pb-3 font-semibold text-md md:text-lg">
-        I am a full-stack developer, who is
-      </Text>
-      <List>
-        <DescriptionListItem text="Super excited about new technologies" />
-        <DescriptionListItem text="Ready to tackle challenges head on" />
-        <DescriptionListItem text="Always willing to learn" />
-      </List>
-    </div>
+    <GroupedTransition
+      mounted={loaded}
+      transitions={{
+        l1: { transition: "slide-left", duration },
+        l2: { transition: "slide-left", duration: duration + 250 },
+        l3: { transition: "slide-left", duration: duration + 500 },
+        t: { transition: "skew-up", duration: 500 },
+      }}
+    >
+      {(styles) => (
+        <div className="flex lg:flex-1 justify-center flex-col border-l pl-6 h-min py-5">
+          <Title
+            style={styles.t}
+            className="h-min text-xl md:text-2xl lg:text-3xl"
+          >
+            Hi, my name is Yash
+          </Title>
+          <Text
+            style={styles.t}
+            className="pb-3 font-semibold text-md md:text-lg"
+          >
+            I am a full-stack developer, who is
+          </Text>
+          <List>
+            <DescriptionListItem
+              style={styles.l1}
+              text="Super excited about new technologies"
+            />
+            <DescriptionListItem
+              style={styles.l2}
+              text="Ready to tackle challenges head on"
+            />
+            <DescriptionListItem
+              style={styles.l3}
+              text="Always willing to learn"
+            />
+          </List>
+        </div>
+      )}
+    </GroupedTransition>
   );
 }
 
-
 interface ListItemProps {
   text: string;
+  style: any;
 }
 function DescriptionListItem(props: ListItemProps) {
   return (
     <List.Item
       className="text-md md:text-lg"
+      style={props.style}
       icon={
         <ThemeIcon color="blue" size={24} radius="xl">
           <CircleCheck size={20} />
@@ -131,36 +163,54 @@ function DescriptionListItem(props: ListItemProps) {
   );
 }
 
-
 function WelcomeText() {
+  let [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
   return (
-    <div className="flex flex-col justify-center mt-12 md:mt-4 lg:flex-[1.5]">
-      <Highlight
-        align="center"
-        highlight={["WhyAsh's"]}
-        highlightStyles={(theme) => ({
-          backgroundImage: theme.fn.linearGradient(
-            45,
-            theme.colors.cyan[5],
-            theme.colors.indigo[5]
-          ),
-          WebkitBackgroundClip: "text",
-          WebkitTextFillColor: "transparent",
-        })}
-        className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-semibold"
-      >
-        Welcome to WhyAsh&apos;s
-      </Highlight>
-      <div className="h-3 md:h-5 lg:h-7 xl:h-9" />
-      <div className="flex justify-center text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-mono text-white">
-        <Typewriter
-          options={{
-            strings: ["dev_life", "blog", "journal", "projects"],
-            autoStart: true,
-            loop: true,
-          }}
-        />
-      </div>
-    </div>
+    <GroupedTransition
+      mounted={loaded}
+      transitions={{
+        t1: { transition: "slide-right", duration: 500 },
+        t2: { transition: "scale", duration: 500 },
+      }}
+    >
+      {(styles) => (
+        <div className="flex flex-col justify-center mt-12 md:mt-4 lg:flex-[1.5]">
+          <Highlight
+            align="center"
+            highlight={["WhyAsh's"]}
+            highlightStyles={(theme) => ({
+              backgroundImage: theme.fn.linearGradient(
+                45,
+                theme.colors.cyan[5],
+                theme.colors.indigo[5]
+              ),
+              WebkitBackgroundClip: "text",
+              WebkitTextFillColor: "transparent",
+            })}
+            className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-semibold"
+            style={styles.t1}
+          >
+            Welcome to WhyAsh&apos;s
+          </Highlight>
+          <div className="h-3 md:h-5 lg:h-7 xl:h-9" />
+          <div
+            className="flex justify-center text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-mono text-white"
+            style={styles.t2}
+          >
+            <Typewriter
+              options={{
+                strings: ["dev_life", "blog", "journal", "projects"],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </div>
+        </div>
+      )}
+    </GroupedTransition>
   );
 }
