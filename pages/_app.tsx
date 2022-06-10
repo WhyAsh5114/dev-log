f7ba14b8158af2714d9e9fa0cb0ead25237805d7
WhyAsh5114 <whyash5114@gmail.com>
2022-06-10 10:39:13 +0530

    added a meta description for seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,7 @@ export default function App(props: AppProps) {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
+        <meta name="description" content="WhyAsh5114's dev-log and portfolio" />
       </Head>
 
       <MantineProvider
