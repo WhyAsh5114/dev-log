f3733f37a510d100d9997c5da8603b0cec4b447b
WhyAsh5114 <whyash5114@gmail.com>
2022-06-10 13:29:26 +0530

    seperated the projects section into a new page also added transitions to background images

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,91 +1,41 @@
 import {
-  Divider,
   Highlight,
-  Transition,
   Title,
   Text,
   List,
   ThemeIcon,
   GroupedTransition,
+  Transition,
 } from "@mantine/core";
 import { CircleCheck } from "tabler-icons-react";
 import { NavBar } from "./../components/navigation/NavBar";
 import Typewriter from "typewriter-effect";
-import Proj1 from "./../data/projects/next_cryptocurrencies_handbook.mdx";
-import Proj2 from "./../data/projects/dota2_rampage_tracker.mdx";
 import { useEffect, useState } from "react";
 
-const components = {
-  h1: (props: any) => (
-    <h2
-      className="font-bold text-xl md:text-2xl lg:text-3xl xl:text-4xl"
-      {...props}
-    />
-  ),
-  p: (props: any) => <p className="text-sm md:text-base" {...props} />,
-};
 
 export default function HomePage() {
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
   return (
     <>
       <div className="flex flex-col h-full">
         <NavBar />
-        <div
-          className="flex h-full items-center gap-2 flex-col md:flex-row justify-evenly"
-          style={{
-            backgroundImage: `url('/layered-waves-haikei.svg')`,
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-          }}
-        >
-          <WelcomeText />
-          <Description />
-        </div>
-      </div>
-
-      <Divider size={"sm"} color="indigo" />
-
-      <div
-        className="overflow-auto min-h-full flex place-items-center justify-center"
-        id="projects"
-        style={{
-          backgroundImage: `url('/layered-peaks-haikei.svg')`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-          backgroundAttachment: "fixed",
-        }}
-      >
-        <div className="flex w-full md:w-2/3 flex-col justify-center p-8 gap-5 ml-auto h-full mb-5 mr-0 md:mr-6 lg:mr-12 xl:mr-20 mt-3">
-          <Highlight
-            highlight={["Projects"]}
-            highlightStyles={(theme) => ({
-              backgroundImage: theme.fn.linearGradient(
-                45,
-                theme.colors.indigo[7],
-                theme.colors.indigo[3]
-              ),
-              WebkitBackgroundClip: "text",
-              WebkitTextFillColor: "transparent",
-            })}
-            className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-semibold px-4 text-right"
+        <Transition mounted={loaded} transition="fade" duration={500}>
+          {(styles) => <div
+            className="flex h-full items-center gap-2 flex-col md:flex-row justify-evenly"
+            style={Object.assign({
+              backgroundImage: `url('/layered-waves-haikei.svg')`,
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
+            }, styles)}
           >
-            My Projects
-          </Highlight>
-          <div className="flex bg-stone-900 rounded-lg flex-col gap-2 p-4 md:p-6 lg:p-8 xl:p-10">
-            <Proj1 components={components} />
-          </div>
-          <Divider size="sm" color="indigo" className="rounded-full my-10" />
-          <div className="flex bg-stone-900 rounded-lg flex-col gap-2 p-4 md:p-6 lg:p-8 xl:p-10">
-            <Proj2 components={components} />
-          </div>
-        </div>
-      </div>
-
-      <Divider size={"sm"} color="indigo" />
-
-      <div className="h-1/6 w-full flex items-center justify-center p-3 gap-2 flex-col bg-gray-900">
-        <Title order={2}>WhyAsh&apos;s Dev-Log</Title>
-        <Text>©️ Copyright 2022</Text>
+            <WelcomeText />
+            <Description />
+          </div>}
+        </Transition>
       </div>
     </>
   );
