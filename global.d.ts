0f1d2e6b9963c880b0f7ec4e739b4b7d1fb0c976
WhyAsh5114 <whyash5114@gmail.com>
2022-06-19 22:27:13 +0530

    added blogMetadata type

--- global.d.ts
+++ global.d.ts
@@ -9,3 +9,12 @@ interface projectMetadata {
     website: string;
     tech_stack: string[];
 }
+
+interface blogMetadata {
+    file_name: string;
+    title: string;
+    description: string;
+    author: string;
+    tags: string[];
+    date: string;
+}
