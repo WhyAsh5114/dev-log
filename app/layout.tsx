3b414165b748b2ef26b99dd22b34293187b7d1bc
WhyAsh5114 <whyash5114@gmail.com>
2024-10-17 17:41:10 +0530

    feat: adding a header

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from "@/components/ThemeProvider";
 import { ModeToggle } from "@/components/ModeToggle";
 import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 
 export const metadata: Metadata = {
   title: "Dev Log",
@@ -23,17 +24,49 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
-      <body className={cn(inter.className, "flex flex-col h-screen w-screen")}>
+      <body
+        className={cn(
+          inter.className,
+          "min-h-screen min-w-full overflow-y-auto overflow-x-hidden"
+        )}
+      >
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
         >
-          <ModeToggle />
+          <DesktopHeader />
+          <MobileHeader />
           {children}
         </ThemeProvider>
       </body>
     </html>
+  );
+}
+
+function DesktopHeader() {
+  return (
+    <header className="sticky top-0 lg:flex gap-2 items-center p-2 border-b bg-background/75 backdrop-blur-sm hidden">
+      <h1 className="font-bold text-xl">Dev-Log</h1>
+      <Button className="ml-8" variant="link">
+        Home
+      </Button>
+      <Button variant="link">Projects</Button>
+      <Button variant="link">Experience</Button>
+      <Button variant="link" className="mr-auto">
+        About
+      </Button>
+      <ModeToggle />
+    </header>
+  );
+}
+
+function MobileHeader() {
+  return (
+    <header className="sticky top-0 flex lg:hidden gap-2 items-center p-2 border-b bg-background/75 backdrop-blur-sm">
+      <h1 className="font-bold text-xl mr-auto">Dev-Log</h1>
+      <ModeToggle />
+    </header>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,78 @@
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <ul>
+        <li className="py-12">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Exercitationem provident odio repellat sint fugit esse, iure corporis
+          dolor ipsum mollitia ut suscipit labore aut dolores consectetur rerum.
+          Dicta, quas aperiam!
+        </li>
+        <li className="py-12">
+          Quod ad unde fuga expedita ipsa, voluptas, quibusdam inventore
+          officiis dolores nostrum culpa soluta ipsum reiciendis exercitationem
+          molestias explicabo aut est accusamus beatae fugit! Reprehenderit
+          dignissimos accusamus praesentium ducimus iste?
+        </li>
+        <li className="py-12">
+          Fuga et voluptatibus rerum magni id labore quibusdam reprehenderit,
+          quo repellat consequatur doloribus ab ullam soluta facilis facere
+          odit. Vero consequuntur, tempore veritatis quo dolores expedita a
+          nobis porro debitis!
+        </li>
+        <li className="py-12">
+          Voluptatem iste ab fugit nihil soluta ea illum accusamus, ipsum eum
+          alias corrupti dolor in magni doloribus fuga exercitationem doloremque
+          eligendi quia. Dolore deleniti expedita, quae excepturi vitae illo
+          voluptas?
+        </li>
+        <li className="py-12">
+          Assumenda tenetur ab provident repellat, consequatur ut. Impedit
+          eveniet nihil vero consequatur blanditiis laudantium laboriosam dolore
+          ratione neque exercitationem. Vitae dolor quidem cum porro. Nam fuga
+          mollitia minus aspernatur excepturi.
+        </li>
+        <li className="py-12">
+          Minus nulla ut, architecto numquam eligendi, modi ab libero temporibus
+          id dolore praesentium impedit maxime aspernatur, neque consequuntur
+          reprehenderit culpa dolores dignissimos? Cumque nobis cupiditate ullam
+          similique odit fugit exercitationem.
+        </li>
+        <li className="py-12">
+          Deserunt repellendus ducimus delectus at commodi dolor ea laudantium
+          qui ipsam sequi ab nemo magnam vero quae obcaecati iusto molestias,
+          nam saepe omnis in adipisci labore. Inventore dolor eius voluptatibus?
+        </li>
+        <li className="py-12">
+          Exercitationem nemo repellendus voluptatum animi similique, fugiat
+          necessitatibus corporis possimus optio explicabo ducimus voluptatibus
+          pariatur laudantium facere architecto atque. Numquam facere quas sit
+          totam voluptatibus voluptatum nihil maxime, beatae blanditiis?
+        </li>
+        <li className="py-12">
+          Repellat aut veniam, vitae a error maxime dolor, assumenda consectetur
+          quis eligendi sed nulla, aperiam debitis non facere voluptas nesciunt
+          vero inventore porro? Maxime nemo nihil molestias vitae rem sunt!
+        </li>
+        <li className="py-12">
+          Facere non assumenda et pariatur provident quia consectetur vero
+          voluptatibus, consequuntur, eaque perferendis nemo numquam sit dolor,
+          ex illum recusandae! Corporis eligendi sed, reprehenderit
+          necessitatibus quaerat animi eius corrupti harum.
+        </li>
+        <li className="py-12">
+          Facilis beatae vero, quia at nostrum voluptatibus autem velit earum
+          vel nemo nulla eum molestiae laudantium nisi delectus veritatis
+          tempora architecto. Aspernatur dignissimos error veritatis. Possimus
+          ex recusandae earum. Magni?
+        </li>
+        <li className="py-12">
+          Inventore, minima rem! Nisi consequuntur quisquam ut, repudiandae
+          consequatur atque, vero soluta quam sit veniam quasi blanditiis alias
+          quibusdam? Voluptatum perspiciatis est similique nemo molestiae vitae
+          cumque animi excepturi ipsum?
+        </li>
+      </ul>
+    </>
+  );
 }
