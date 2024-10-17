9ed4f38873726045893fbd6150f280f2891ae474
WhyAsh5114 <whyash5114@gmail.com>
2024-10-17 17:08:24 +0530

    feat: inter font

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,18 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { ModeToggle } from "@/components/ModeToggle";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
   title: "Dev Log",
   description: "WhyAsh5114's dev log",
 };
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
@@ -16,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
-      <body>
+      <body className={cn(inter.className, "flex flex-col h-screen w-screen")}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
