b9e8e69163d01bb48cb3d8cc96cd4141ed0defed
WhyAsh5114 <whyash5114@gmail.com>
2024-10-18 17:47:32 +0530

    feat: setup unplugin-icons for react and nextjs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
+import Icons from "unplugin-icons/webpack";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    config.plugins.push(
+      Icons({
+        compiler: "jsx",
+        jsx: "react",
+      })
+    );
+    return config;
+  },
 };
 
 export default nextConfig;
