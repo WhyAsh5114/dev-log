dbb7fbc99f343f7c2e8a19d3fd8acf44558989a4
WhyAsh5114 <whyash5114@gmail.com>
2024-10-19 17:47:27 +0530

    feat: add project grid and cards

---
+++ app/projects/components/ProjectCard.tsx
@@ -0,0 +1,41 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { ExternalLink } from "lucide-react";
+import Link from "next/link";
+import GitHub from "~icons/mdi/github";
+import TechStack from "../../components/TechStack";
+import { Project } from "../data";
+
+export default function ProjectCard({ project }: { project: Project }) {
+  return (
+    <>
+      <Card>
+        <CardHeader>
+          <CardTitle>{project.name}</CardTitle>
+          <CardDescription>{project.description}</CardDescription>
+        </CardHeader>
+        <CardContent className="grid">
+          <TechStack techStack={project.techStack} />
+        </CardContent>
+        <CardFooter className="justify-end gap-4">
+          <Link href={`https://github.com/WhyAsh5114/${project.name}`}>
+            <GitHub className="h-6 w-6" />
+          </Link>
+          {project.link && (
+            <Link href={project.link}>
+              <ExternalLink />
+            </Link>
+          )}
+          <Button size="sm">View more</Button>
+        </CardFooter>
+      </Card>
+    </>
+  );
+}
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,9 +1,16 @@
 import { TypographyH1 } from "@/components/ui/typographyH1";
+import ProjectCard from "./components/ProjectCard";
+import { allProjects } from "./data";
 
 export default function Projects() {
   return (
     <>
       <TypographyH1>Projects</TypographyH1>
+      <div className="grid sm:grid-cols-2 mt-6 gap-1">
+        {allProjects.map((project) => (
+          <ProjectCard key={project.name} project={project} />
+        ))}
+      </div>
     </>
   );
 }
