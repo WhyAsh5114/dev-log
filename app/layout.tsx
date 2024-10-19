f378051ff7e410a253b3ac858a3b499016e8741a
WhyAsh5114 <whyash5114@gmail.com>
2024-10-19 11:13:48 +0530

    refactor: combined small components and removed unnecessary ones

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import { ModeToggle } from "@/components/ModeToggle";
 import { ThemeProvider } from "@/components/ThemeProvider";
+import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import NavLinks from "./components/NavLinks";
+import Link from "next/link";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -45,9 +46,19 @@ export default function RootLayout({
 }
 
 function Header() {
+  const links: { text: string; href: string }[] = [
+    { text: "Home", href: "/" },
+    { text: "Projects", href: "/projects" },
+    { text: "Work", href: "/work" },
+  ];
+
   return (
     <header className="sticky top-0 flex items-center p-2 bg-background/75 backdrop-blur-md max-w-screen-sm mx-auto">
-      <NavLinks className="font-semibold" />
+      {links.map(({ href, text }) => (
+        <Button key={text} className="font-semibold" variant="link" asChild>
+          <Link href={href}>{text}</Link>
+        </Button>
+      ))}
       <ModeToggle />
     </header>
   );
