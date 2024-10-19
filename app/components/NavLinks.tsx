8ac940793c7cd4a7c84100096eec4e967ebdb7bb
WhyAsh5114 <whyash5114@gmail.com>
2024-10-19 10:48:03 +0530

    fix: no longer need NavLinks to be a client component

--- app/components/NavLinks.tsx
+++ app/components/NavLinks.tsx
@@ -1,16 +1,8 @@
-"use client";
-
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 
-export default function NavLinks({
-  className,
-  onClick,
-}: {
-  className?: string;
-  onClick?: () => void;
-}) {
+export default function NavLinks({ className }: { className?: string }) {
   const links: { text: string; href: string }[] = [
     { text: "Home", href: "/" },
     { text: "Projects", href: "/projects" },
@@ -18,13 +10,7 @@ export default function NavLinks({
   ];
 
   return links.map(({ href, text }) => (
-    <Button
-      key={text}
-      className={cn(className)}
-      onClick={onClick}
-      variant="link"
-      asChild
-    >
+    <Button key={text} className={cn(className)} variant="link" asChild>
       <Link href={href}>{text}</Link>
     </Button>
   ));
