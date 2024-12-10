0efb6c066ed0f4257f028e1b35ea2d3ec3a6453b
WhyAsh5114 <whyash5114@gmail.com>
2024-12-10 19:24:09 +0530

    fix: projects with special characters don't work in static export for some reason

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 import Icons from "unplugin-icons/webpack";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  images: { unoptimized: true },
   webpack(config) {
     config.plugins.push(
       Icons({
